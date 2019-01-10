98c84b87f7ea202d29dabe7949b176f904a626d1
unknown <anderqui001@warren.k12.in.us>
2019-01-10 12:58:01 -0500

    notes

---
+++ variables/lesson1.js
@@ -0,0 +1,18 @@
+/* GLOBAL VARIABLES- THE VARIABLE CAN BE USED THROUGHT THE ENITRE SOCUMENT/SCRIPT
+/*LOCAL VARIBALE- THE VARIABLE CAN ONLY BE USED IN THE SECTION IT WAS DEFINED
+*/
+$(document).ready(function(){
+
+    alert("hi");
+});
+
+var userInput;
+
+function getInfo(){
+    userInput="Hi";
+}
+
+function showInfo(){
+
+    alert("Hi");
+})
