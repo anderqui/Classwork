7fd1bbdc6d8f6a848cb04e8682b399e80046bdc2
unknown <anderqui001@warren.k12.in.us>
2019-01-14 13:07:32 -0500

    initial

--- variables/lesson1.js
+++ variables/lesson1.js
@@ -1,18 +1,27 @@
-/* GLOBAL VARIABLES- THE VARIABLE CAN BE USED THROUGHT THE ENITRE SOCUMENT/SCRIPT
-/*LOCAL VARIBALE- THE VARIABLE CAN ONLY BE USED IN THE SECTION IT WAS DEFINED
+/*
+GLOBAL VARIABLES- THE VARIABLE CAN BE USED THROUGHT THE ENITRE SOCUMENT/SCRIPT
+LOCAL VARIBALE- THE VARIABLE CAN ONLY BE USED IN THE SECTION IT WAS DEFINED
 */
+/*$(document).ready(function(){
+    getInfo();
+    showInfo("hi");
+})*/
 $(document).ready(function(){
-
-    alert("hi");
+    $("#button").click(function(){
+            showInfo();
+    })
 });
 
-var userInput;
+var userName;
 
 function getInfo(){
-    userInput="Hi";
+   userName= $("#userName").val();
 }
 
 function showInfo(){
-
-    alert("Hi");
-})
+    getInfo();
+    var message= "Hello " +userName +", glad to meet you!";
+    
+  $("#output").text(message).fadeIn("slow").animate({"top":"0px"},"slow");
+  $("#output").text(message).show().animate({"top":"0px", "opacity":"1"},"slow");
+}
