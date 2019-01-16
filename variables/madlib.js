93000e3518547998d354be8caef002f74390f655
unknown <anderqui001@warren.k12.in.us>
2019-01-16 13:31:01 -0500

    madlibupdate 2.0

---
+++ variables/madlib.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#button").click(function(){
+        showInfo();
+    })
+});
+var nounUno;
+var nounDos;
+var nounTres;
+var nounQuatro;
+var nounCinco;
+var
+
+function getInfo(){
+    noun
+
+}
